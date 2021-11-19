e088f37fbae656768562dfeb92b9cd06d473fc88
unknown <srevar@pdx.edu>
2021-11-18 16:56:54 -0800

    'HW'

--- hw4/01-characters.js
+++ hw4/01-characters.js
@@ -1,4 +1,45 @@
 // URL to Game of Thrones API to fetch all characters
-let url = 'https://thronesapi.com/api/v2/Characters';
+const url = 'https://thronesapi.com/api/v2/Characters';
 
-fetch(url);
+const app = document.querySelector('#characters');
+
+const addCharactertoDOM = (character) => {
+
+    let element = document.createElement('div');
+    let fName = document.createElement('div');
+    let title = document.createElement('div');
+    let image = document.createElement('div');
+
+    fName.textContent = character.firstName;
+    title.textContent = character.title;
+    image.textContent = character.image;
+
+    element.append(fName);
+    element.append(title);
+    element.append(image);
+
+    app.append(element);
+
+}
+
+const fetchData = (url) => {
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+
+            data.forEach((character) => {
+                console.log(character.fullName, 
+                            character.title, 
+                            character.image,
+                            character.imageUrl,
+                            );
+                            addCharactertoDOM(character);
+            });
+        })
+
+        .catch((error) => console.error(error));
+};
+
+fetchData(url);
